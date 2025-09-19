cc6565142065659bb69bae428c92c09d3c17f256
Ty'Ryan Craft <tyryancraft@gmail.com>
2025-09-19 09:49:55 -0400

    CSC120_Project_F25_C0C2D

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+# CSC 120 Project Part 1
+# Starter game code
+
+game_name = "Escaping"  
+
+# Greeting with aligned equal signs
+print("Welcome to " + game_name + "!")
+print("====================")
+
+# Ask for the character's name
+print("Before we begin, what is your character's name?")
+name = input("> ")
+
+# Print the personalized greeting
+print("Great, " + name + "! Let's begin the adventure!")
+
